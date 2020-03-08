309422676d6313c55ea43094ed9ab2d336d97a16
koooge <koooooge@gmail.com>
2020-03-08 13:13:29 +0100

    Toast Hello AAR

--- app/src/main/java/com/example/helloaar/MainActivity.kt
+++ app/src/main/java/com/example/helloaar/MainActivity.kt
@@ -8,7 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
         val helloAAR = HelloAAR()
-        helloAAR.hello()
+        helloAAR.hello(this.applicationContext)
     }
 }
--- helloaar/src/main/java/com/example/helloaar/HelloAAR.kt
+++ helloaar/src/main/java/com/example/helloaar/HelloAAR.kt
@@ -1,11 +1,14 @@
 package com.example.helloaar
 
+import android.content.Context;
 import android.util.Log
+import android.widget.Toast
 
 class HelloAAR {
     private val TAG = "HelloAAR"
 
-    fun hello() {
-     Log.i(TAG, "Hello AAR!")
+    fun hello(con: Context) {
+        Log.i(TAG, "Hello AAR!")
+        Toast.makeText(con, "Hello AAR!", Toast.LENGTH_SHORT).show()
     }
  }
